afebf72215a65e987d12342f435a9bd73c2a50c0
dima.bogatyrev.2001@gmail.com <123zxc456vbnSQ>
2024-03-29 07:25:22 +0700

    Create Project

---
+++ BeautySaloon/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿const body = document.querySelector("body"),
+    sidebar = document.querySelector(".sidebar"),
+    toggle = document.querySelector('.toggle'),
+    searchBtn = body.querySelector('.search-box'),
+    modeSwitch = body.querySelector('.toggle-switch'),
+    modeText = body.querySelector('.mode-text');
+
+toggle.addEventListener("click", () =>
+{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () =>
+{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click", () =>
+{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark"))
+    {
+        modeText.innerText = "День";
+    }
+    else
+    {
+        modeText.innerText = "Ночь";
+    }
+})
