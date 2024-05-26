f9754a5ea70b389e300a33fbee22730254233eaa
dima.bogatyrev.2001@gmail.com <123zxc456vbnSQ>
2024-05-27 03:56:20 +0700

    Картинка тех работ + миграции

---
+++ BeautySaloon/Migrations/20240526194243_deleteIsActive.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautySaloon.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "UserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Caregories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageSrc",
+                table: "Services",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "UserTokens",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Sessions",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Services",
+                keyColumn: "ImageSrc",
+                keyValue: null,
+                column: "ImageSrc",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageSrc",
+                table: "Services",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Services",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Roles",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Caregories",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
