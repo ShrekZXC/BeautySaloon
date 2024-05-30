8613dcd46967e57972903498d8e3de1092eb8356
dima.bogatyrev.2001@gmail.com <123zxc456vbnSQ>
2024-05-30 07:28:02 +0700

    Создал сущность встречи ( записи на услугу )

---
+++ BeautySaloon/DAL/Entity/AppointmentEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace BeautySaloon.DAL.Entity;
+
+public class AppointmentEntity
+{
+    public Guid ClientId { get; set; }
+    public ApplicationUser Client { get; set; }
+    
+    public Guid MasterId { get; set; }
+    public ApplicationUser Master { get; set; }
+    
+    public Guid ServiceId { get; set; }
+    public ServiceEntity Service { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
---
+++ BeautySaloon/ViewModel/AppointmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BeautySaloon.ViewModel;
+
+public class AppointmentViewModel
+{
+    public Guid ClientId { get; set; }
+    public UserViewModel Client { get; set; }
+    public Guid MasterId { get; set; }
+    public UserViewModel Master { get; set; }
+    public Guid ServiceId { get; set; }
+    public ServiceViewModel Service { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
